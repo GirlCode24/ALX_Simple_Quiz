a3b43dfb29e537b23f8cda88ecc1c0fc90665b06
GirlCode24 <janetquayenaa@gmail.com>
2025-08-03 23:05:40 +0000

    event listener for submit button added

--- quiz.js
+++ quiz.js
@@ -1,20 +1,21 @@
 function checkAnswer() {
   let correctAnswer = "4";
-  
 
   const selectedOption = document.querySelector('input[name="quiz"]:checked');
-  
 
   let userAnswer = selectedOption ? selectedOption.value : null;
 
   console.log("User Answer:", userAnswer);
   console.log("Correct Answer:", correctAnswer);
 
-  const feedback = document.getElementById("feedback")
-  if (userAnswer === correctAnswer){
+  const feedback = document.getElementById("feedback");
+  if (userAnswer === correctAnswer) {
     feedback.textContent = "Correct! Well done.";
+  } else {
+    feedback.textContent = "That's incorrect. Try again!";
   }
-  else{
-    feedback.textContent ="That's incorrect. Try again!";
-  }
+
+  const submitButton = document.getElementById("submit-answer");
+
+  submitButton.addEventListener("click", checkAnswer);
 }
